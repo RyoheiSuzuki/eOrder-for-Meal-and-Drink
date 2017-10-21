6477df0e4d142fb254712e58ff4007da22a28cc6
RyoheiSuzuki <ryohei.s.2210@gmail.com>
2017-10-21 10:23:40 +0700

    a
    a

---
+++ eOrder for Meal and Drink/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace eOrder_for_Meal_and_Drink
+{
+    [Activity(Label = "eOrder_for_Meal_and_Drink", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
