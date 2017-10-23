d5226522cdfe2bf17f46e3e59d85b10e65144a88
RyoheiSuzuki <ryohei.s.2210@gmail.com>
2017-10-24 01:59:28 +0700

    update

--- eOrder for Meal and Drink/MainActivity.cs
+++ eOrder for Meal and Drink/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 namespace eOrder_for_Meal_and_Drink
 {
@@ -13,6 +14,28 @@ namespace eOrder_for_Meal_and_Drink
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            // Add Toolbar
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = "eOrder";
+
+            // Set Top Image View
+            var imageView = FindViewById<ImageView>(Resource.Id.ImageViewTop);
+            imageView.SetImageResource(Resource.Drawable.pic_top);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+                ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
